c607933ae204eff96fd56b0a2ee05f3a16f05144
furqee <sm.furqankhan@gmail.com>
2023-03-27 02:58:01 +0500

    Genre migration, controller, model and factory added.

---
+++ database/migrations/2023_03_27_203224_create_films_genres_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('films_genres', function (Blueprint $table) {
+            $table->unsignedBigInteger('film_id')->index();
+            $table->unsignedBigInteger('genre_id')->index();
+
+            $table->foreign('film_id')->references('id')->on('films')->onDelete('cascade');
+            $table->foreign('genre_id')->references('id')->on('genres')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('films_genres');
+    }
+};
