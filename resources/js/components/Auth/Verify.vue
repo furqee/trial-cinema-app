51963ae5898c0f6fff5292d094be715c62fe32d7
furqee <sm.furqankhan@gmail.com>
2023-03-27 04:11:23 +0500

    Code aligned

--- resources/js/components/Auth/Verify.vue
+++ resources/js/components/Auth/Verify.vue
@@ -1,41 +1,41 @@
 <template>
-  <div class="verify">
-    <div class="alert alert-danger" role="alert" v-if="error">
-      {{ error }}
+    <div class="verify">
+        <div class="alert alert-danger" role="alert" v-if="error">
+            {{ error }}
+        </div>
+        <h1 v-show="!error">Please wait..</h1>
     </div>
-    <h1 v-show="!error">Please wait..</h1>
-  </div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from "vuex";
+import {mapGetters, mapActions} from "vuex";
 
 export default {
-  props: ["hash"],
+    props: ["hash"],
 
-  data() {
-    return {
-      error: null
-    };
-  },
+    data() {
+        return {
+            error: null
+        };
+    },
 
-  mounted() {
-    this.sendVerifyRequest(this.hash)
-      .then(() => {
-          this.$router.push({ name: "Films" });
-      })
-      .catch(error => {
-        console.log(error.response);
-        this.error = "Error verifying email";
-      });
-  },
+    mounted() {
+        this.sendVerifyRequest(this.hash)
+            .then(() => {
+                this.$router.push({name: "Films"});
+            })
+            .catch(error => {
+                console.log(error.response);
+                this.error = "Error verifying email";
+            });
+    },
 
-  computed: {
-    ...mapGetters("auth", ["user"])
-  },
+    computed: {
+        ...mapGetters("auth", ["user"])
+    },
 
-  methods: {
-    ...mapActions("auth", ["sendVerifyRequest"])
-  }
+    methods: {
+        ...mapActions("auth", ["sendVerifyRequest"])
+    }
 };
 </script>
